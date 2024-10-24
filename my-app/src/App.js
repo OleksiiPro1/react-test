2a46df93121ca1ea02f4a0719c33a757f1617930
Alexey Piltenko <ukr.top@ukr.net>
2024-10-24 19:10:29 +0200

    Homework (fetch the store)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Counter from './Components/Counter/Counter';
 import Homework from './Components/Homework';
 import PokemonApi from './Components/PokemonApi/PokemonApi';
+import Store from './Components/Store/Store';
 
 function App() {
   const [state, setstate] = useState(true);
   return (
     <div className="App">
+      <Store />
       {/* <Counter />
       <Homework /> */}
-      {state && <PokemonApi/>}
+      {/* {state && <PokemonApi/>}
       <button onClick={()=>{
 setstate(false)
-      }}>click delete</button>
+      }}>click delete</button> */}
     </div>
   );
 }
---
+++ my-app/src/Components/Store/Store.jsx
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react';
+
+const Store = () => {
+    const [offset, setOffset] = useState(0);
+const [productsList, setProductsList] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=1`)
+        .then((res) => res.json())
+        .then((data) => {
+            setProductsList((prod) => [...prod, ...data]);
+        })
+        .catch((error) => {
+            console.error('Error fetching products:', error);
+        });
+}, [offset]); 
+    return (
+        <div>
+            <div>
+               
+                    
+                    {productsList.map((product)=>{
+                        return (
+                    <div>
+                        <h1 key={product.id}><b>Title: </b>{product.title}</h1>
+
+                        <p><b>Prise:</b> {product.price}</p>
+ 
+                        <p><b>Description:</b> {product.description}</p>
+
+                        <p><b>Product category id:</b> {product.category.id}</p>
+
+                        <p><b>Product category name:</b> {product.category.name}</p>
+
+                        <img src={product.category.image} width="200" />
+                    </div>
+                    )                      
+                    })}
+                   
+                
+
+                <button onClick={()=>{
+                    setOffset(offset+1)
+                }}>next page</button>
+            </div>
+        </div>
+    );
+    
+}
+
+export default Store;
