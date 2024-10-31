3d8f28fc62346ef371d87a4271b2de3d85bc0cac
Alexey Piltenko <ukr.top@ukr.net>
2024-10-31 19:08:28 +0100

    New labels and inputs

--- my-app/src/Components/Contacts/Contacts.jsx
+++ my-app/src/Components/Contacts/Contacts.jsx
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Contacts = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
     const [inputValue, setInputValue] = useState('');
     const [contacts, setContacts] = useState(() => {
     const localSave = JSON.parse(localStorage.getItem('contacts'))
@@ -19,33 +20,41 @@ const Contacts = () => {
     function handleInputPhoneNumber(e) {
         setPhoneNumber(e.target.value);
     }
+    function handleInputEmail(e) {
+        setEmail(e.target.value);
+    }
     function handleSubmit(e) {
         e.preventDefault();
-        if (!inputValue || !phoneNumber) {
+        if (!inputValue || !phoneNumber || !email) {
             alert('All fields must be filled out!');
             return;
         }
         
         setContacts((prev) => {
-            return [...prev, { id: uuidv4(), name: inputValue, phone: phoneNumber }]
+            return [...prev, { id: uuidv4(), name: inputValue, phone: phoneNumber, email: email }]
         });
         
          
 
-        setInputValue('')
-        setPhoneNumber('')
+        setInputValue('');
+        setPhoneNumber('');
+        setEmail('');
     }
     return (
         <div>
             <form action="submit" onSubmit={handleSubmit}>
             <br />
+                <label>Name </label><br />
                 <input onChange={handleInput} value={inputValue} type="text" /><br /><br />
+                <label>Phone number </label><br />
                 <input onChange={handleInputPhoneNumber} value={phoneNumber} type="text" /><br /><br />
+                <label>Email </label><br />
+                <input onChange={handleInputEmail} value={email} type="text" /><br /><br />
                 <button type='submit'>Add a contact</button>
             </form>
             <ul>
                 {contacts.map((contact) =>{
-                    return (<li key={contact.id}>{contact.name}: {contact.phone}</li>)
+                    return (<li key={contact.id}>{contact.name}: {contact.phone} / {contact.email}</li>)
                 })}
             </ul>
         </div>
