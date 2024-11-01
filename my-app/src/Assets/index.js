2ea3c8d84448b731dd1b9ebcba6bcb4b14699cd8
Alexey Piltenko <ukr.top@ukr.net>
2024-11-01 18:18:23 +0100

    Homework with an error

---
+++ my-app/src/Assets/index.js
@@ -0,0 +1,4 @@
+import closeImg from './close.png';
+import penImg from './pen.png';
+
+export default {closeImg, penImg};
--- my-app/src/Components/Contacts/Contacts.jsx
+++ my-app/src/Components/Contacts/Contacts.jsx
@@ -1,7 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import images from '../../Assets/index';
+import s from './Contacts.module.css';
 
 const Contacts = () => {
+    const [modalInput, setModalInput] = useState('');
+    const [modalPhoneNumber, setModalPhoneNumber] = useState('');
+    const [modalEmail, setModalEmail] = useState('');
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [currentContactChange, setCurrentContactChange] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [email, setEmail] = useState('');
     const [inputValue, setInputValue] = useState('');
@@ -31,7 +38,7 @@ const Contacts = () => {
         }
         
         setContacts((prev) => {
-            return [...prev, { id: uuidv4(), name: inputValue, phone: phoneNumber, email: email }]
+            return [...prev, { id: uuidv4(), name: inputValue, phone: phoneNumber, email }]
         });
         
          
@@ -40,23 +47,81 @@ const Contacts = () => {
         setPhoneNumber('');
         setEmail('');
     }
+
+    function modalHandlSubmit(e) {
+        e.preventDefault();
+        if(!modalInput || !modalPhoneNumber || !modalEmail) {
+            alert('All fields must be filled out!');
+            return;
+        }
+
+        setContacts((prev) => {
+            prev.map((contact) => {
+                contact.id === 1 ? { ...contact, name: modalInput, phone: modalPhoneNumber, email: modalEmail } : contact
+            })
+        });
+        
+         
+
+        setModalInput('');
+        setModalPhoneNumber('');
+        setModalEmail('');
+
+    }
+
+    function modalHandleInput(e) {
+        setInputValue(e.target.value);
+    }
+    function modalHandleInputPhoneNumber(e) {
+        setPhoneNumber(e.target.value);
+    }
+    function modalHandleInputEmail(e) {
+        setEmail(e.target.value);
+    }
+
+    function onClose() {
+        setIsOpenModal(false)
+    }
+
+    function onDelete(id) {
+        console.log(id);
+        const filteredArray = contacts.filter((contact) => {
+            return id !== contact.id;
+        });
+        setContacts(filteredArray);
+    }
     return (
         <div>
             <form action="submit" onSubmit={handleSubmit}>
             <br />
                 <label>Name </label><br />
-                <input onChange={handleInput} value={inputValue} type="text" /><br /><br />
+                <input onChange={modalHandleInput} value={inputValue} type="text" /><br /><br />
                 <label>Phone number </label><br />
-                <input onChange={handleInputPhoneNumber} value={phoneNumber} type="text" /><br /><br />
+                <input onChange={modalHandleInputPhoneNumber} value={phoneNumber} type="text" /><br /><br />
                 <label>Email </label><br />
-                <input onChange={handleInputEmail} value={email} type="text" /><br /><br />
-                <button type='submit'>Add a contact</button>
+                <input onChange={modalHandleInputEmail} value={email} type="text" /><br /><br />
+                <button type='submit' onSubmit={handleSubmit}>Add a contact</button>
             </form>
             <ul>
                 {contacts.map((contact) =>{
-                    return (<li key={contact.id}>{contact.name}: {contact.phone} / {contact.email}</li>)
+                    return (<li key={contact.id}><p>{contact.name}: {contact.phone} / {contact.email}</p><img onClick={() => {
+                        setIsOpenModal(true);
+                        setCurrentContactChange(contact.id);
+                    }} style={{width: '20px'}} src={images.penImg} alt="pen" /><img onClick={() => {onDelete(contact.id)}} style={{width: '20px'}} src={images.closeImg} alt="close" /></li>)
                 })}
             </ul>
+            {isOpenModal && <div className={s.overlay}>
+                <div className={s.modal}>
+                    <form action="submit" onSubmit={modalHandlSubmit}>
+                        <input type="text" onChange={handleInput} />
+                        <input type="text" onChange={handleInputPhoneNumber} />
+                        <input type="text" onChange={handleInputEmail} />
+                        <button type='submit' onSubmit={modalHandlSubmit}>Edit</button>
+                        
+                        <img onClick={() => {onClose()}} style={{width: '20px'}} src={images.closeImg} alt="close" />
+                    </form>
+                </div>
+            </div>}
         </div>
     );
 }
